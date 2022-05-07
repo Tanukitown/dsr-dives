be995580cecfffd6c753143efac815ecaef3a6ff
Tanukitown <kevin.mcguffin@gmail.com>
2022-05-06 22:54:14 -0400

    Add world markers

--- src/components/RoleIcons.svelte
+++ src/components/RoleIcons.svelte
@@ -7,6 +7,14 @@
   const meleeIcon = 'images/Melee_DPS_Icon_Flat_1.png';
   const magicRangedIcon = 'images/Magic_Ranged_DPS_Icon_Flat_1.png';
   const physRangedIcon = 'images/Physical_Ranged_DPS_Flat_Icon_1.png';
+  const one = 'images/waymark1.png';
+  const two = 'images/waymark2.png';
+  const three = 'images/waymark3.png';
+  const four = 'images/waymark4.png';
+  const a = 'images/waymarkA.png';
+  const b = 'images/waymarkB.png';
+  const c = 'images/waymarkC.png';
+  const d = 'images/waymarkD.png';
 
   let target;
   let tankOneFrame = {
@@ -49,6 +57,46 @@
     rotate: 0,
     transformOrigin: "50% 50%",
   }
+  let oneFrame = {
+    translate: [0,0],
+    rotate: 0,
+    transformOrigin: "50% 50%",
+  }
+  let twoFrame = {
+    translate: [0,0],
+    rotate: 0,
+    transformOrigin: "50% 50%",
+  }
+  let threeFrame = {
+    translate: [0,0],
+    rotate: 0,
+    transformOrigin: "50% 50%",
+  }
+  let fourFrame = {
+    translate: [0,0],
+    rotate: 0,
+    transformOrigin: "50% 50%",
+  }
+  let aFrame = {
+    translate: [0,0],
+    rotate: 0,
+    transformOrigin: "50% 50%",
+  }
+  let bFrame = {
+    translate: [0,0],
+    rotate: 0,
+    transformOrigin: "50% 50%",
+  }
+  let cFrame = {
+    translate: [0,0],
+    rotate: 0,
+    transformOrigin: "50% 50%",
+  }
+  let dFrame = {
+    translate: [0,0],
+    rotate: 0,
+    transformOrigin: "50% 50%",
+  }
   const selectedFrame = (target: HTMLElement) => {
     switch (target.id) {
       case 'tankOne':
@@ -67,6 +115,22 @@
         return magicRangedFrame
       case 'physRanged':
         return physRangedFrame
+        case 'oneMarker':
+        return oneFrame
+      case 'twoMarker':
+        return twoFrame
+      case 'threeMarker':
+        return threeFrame
+      case 'fourMarker':
+        return fourFrame
+      case 'aMarker':
+        return aFrame
+      case 'bMarker':
+        return bFrame
+      case 'cMarker':
+        return cFrame
+      case 'dMarker':
+        return dFrame
     }
   };
   const setTarget = (e: Event) => {
@@ -95,46 +159,66 @@
     <img src={tankIcon} alt="tank icon" class='icon-sizing' />
   </div>
 
-  <div class="mt-2" />
-
   <div  id="tankTwo" class="position-relative character-with-debuffs text-center" on:click={setTarget}>
     <img src={tankIcon} alt="tank icon" class='icon-sizing' />
   </div>
-
-  <div class="mt-2" />
 
   <div  id="healerOne" class="position-relative character-with-debuffs text-center" on:click={setTarget}>
     <img src={healerIcon} alt="healer icon" class='icon-sizing' />
   </div>
 
-  <div class="mt-2" />
-
   <div  id="healerTwo" class="position-relative character-with-debuffs text-center" on:click={setTarget}>
     <img src={healerIcon} alt="healer icon" class='icon-sizing' />
   </div>
-
-  <div class="mt-2" />
 
   <div  id="meleeOne" class="position-relative character-with-debuffs text-center" on:click={setTarget}>
     <img src={meleeIcon} alt="melee icon" class='icon-sizing' />
   </div>
 
-  <div class="mt-2" />
-
   <div id="meleeTwo" class="position-relative character-with-debuffs text-center" on:click={setTarget}>
     <img src={meleeIcon} alt="melee icon" class='icon-sizing' />
   </div>
-
-  <div class="mt-2" />
 
   <div id="magicRanged" class="position-relative character-with-debuffs text-center" on:click={setTarget}>
     <img src={magicRangedIcon} alt="magic ranged icon" class='icon-sizing' />
   </div>
 
-  <div class="mt-2" />
-
   <div id="physRanged" class="position-relative character-with-debuffs text-center" on:click={setTarget}>
     <img src={physRangedIcon} alt="phys ranged icon" class='icon-sizing' />
+  </div>
+</div>
+
+<div class="d-flex justify-content-between position-absolute start-0 bottom-0 w-100">
+  <div id="oneMarker" class="position-relative world-marker text-center" on:click={setTarget}>
+    <img src={one} alt="waymark one" class='icon-sizing' />
+  </div>
+
+  <div  id="twoMarker" class="position-relative world-marker text-center" on:click={setTarget}>
+    <img src={two} alt="wayark two" class='icon-sizing' />
+  </div>
+
+  <div  id="threeMarker" class="position-relative world-marker text-center" on:click={setTarget}>
+    <img src={three} alt="waymark three" class='icon-sizing' />
+  </div>
+
+  <div  id="fourMarker" class="position-relative world-marker text-center" on:click={setTarget}>
+    <img src={four} alt="waymark four" class='icon-sizing' />
+  </div>
+
+  <div  id="aMarker" class="position-relative world-marker text-center" on:click={setTarget}>
+    <img src={a} alt="waymark a" class='icon-sizing' />
+  </div>
+
+  <div id="bMarker" class="position-relative world-marker text-center" on:click={setTarget}>
+    <img src={b} alt="waymark b" class='icon-sizing' />
+  </div>
+
+  <div id="cMarker" class="position-relative world-marker text-center" on:click={setTarget}>
+    <img src={c} alt="waymark c" class='icon-sizing' />
+  </div>
+
+  <div id="dMarker" class="position-relative world-marker text-center" on:click={setTarget}>
+    <img src={d} alt="waymark d" class='icon-sizing' />
   </div>
 </div>
 
