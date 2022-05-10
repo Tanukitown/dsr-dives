9fa1349e1316b327caab4f22aead24d8993dc5a9
Kevin McGuffin <kevin.mcguffin@binarydefense.com>
2022-05-10 10:51:48 -0400

    No mobile users madge

--- src/components/Wyrmholes.svelte
+++ src/components/Wyrmholes.svelte
@@ -1095,6 +1095,8 @@
   };
 
   onMount(() => {
+    const mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+    if (mobile) document.getElementsByTagName('body')[0].innerHTML = '<center>This no work on mobile, sorry.</center>';
     initEvents();
   });
 </script>
